7e2f43004decf337e9e1d55af58e250aaa4bb890
sr1k3sh <sr1k3sh@gmail.com>
2023-07-18 16:37:48 +0900

    code refactored

--- src/app/components/headerTitleDesc/index.tsx
+++ src/app/components/headerTitleDesc/index.tsx
@@ -15,7 +15,9 @@ export default function HeaderTitleDesc({ }: Props) {
       <span className={styles.subTitle}>My blogs</span>
       <h1 className={`${styles.title} ${righteous.className}`}>Writing from my experience</h1>
       <p className={styles.description}>
-        is a captivating blog where personal stories and insightful reflections come together to offer a unique and relatable perspective. Join the author on a journey through their triumphs, challenges, and the valuable life lessons they've learned along the way. Discover inspiration, entertainment, and thought-provoking insights that invite you to reflect on your own experiences.
+        {
+          `is a captivating blog where personal stories and insightful reflections come together to offer a unique and relatable perspective. Join the author on a journey through their triumphs, challenges, and the valuable life lessons they've learned along the way. Discover inspiration, entertainment, and thought-provoking insights that invite you to reflect on your own experiences.`
+        }
       </p>
     </div>
   )
